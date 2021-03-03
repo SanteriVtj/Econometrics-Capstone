502c9014693ba77e190cd65d1e9ed13f501cd3e1
SanteriVtj <santeri.vaataja@gmail.com>
2021-03-03 11:15:13 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Coursework for Aalto-university Econometrics course.',
+    author='Santeri Väätäjä',
+    license='',
+)
